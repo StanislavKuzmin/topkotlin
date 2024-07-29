0a8413bf54be36226c2e4af7b1a258ac01915785
Stanislav Kuzmin <stason1986@mail.ru>
2024-07-29 13:04:18 +0300

    first comit

---
+++ src/main/kotlin/com/example/topkotlin2/Topkotlin2Application.kt
@@ -0,0 +1,11 @@
+package com.example.topkotlin2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Topkotlin2Application
+
+fun main(args: Array<String>) {
+    runApplication<Topkotlin2Application>(*args)
+}
